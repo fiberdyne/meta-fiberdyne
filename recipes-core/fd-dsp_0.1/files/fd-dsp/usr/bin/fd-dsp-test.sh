25127a310c3249a9ec4db18884b7eb69c4d67eb5
Builder <devel@45f28166fed1>
2017-01-24 03:03:11 +0100

    Fix UI connection bug, automated audio playback, updated README
    Former-commit-id: a4d2e3aaac18abbb2a2bfd3a6319a3a659752bad

--- recipes-core/fd-dsp_0.1/files/fd-dsp/usr/bin/fd-dsp-test.sh
+++ recipes-core/fd-dsp_0.1/files/fd-dsp/usr/bin/fd-dsp-test.sh
@@ -1,3 +1,6 @@
 # Test direct into hardware buffer sink, avoiding mixer
 ak41_start &
-aplay -Dplug:sinkStereo -c2 -fS16_LE /usr/share/fd-dsp-test.wav
+
+while true; do
+  aplay -Dplug:sinkStereo -c2 -fS16_LE /usr/share/fd-dsp-test.wav
+done
--- recipes-core/fd-dsp_0.1/files/fd-dsp/usr/bin/fd-dsp.sh
+++ recipes-core/fd-dsp_0.1/files/fd-dsp/usr/bin/fd-dsp.sh
@@ -2,4 +2,6 @@ amixer set -c 0 Headphone\ Enable 25 unmute
 amixer set -c 0 LINEOUT\ Mixer\ DACL 25 unmute
 insmod /lib/modules/fd-xtensa-hifi.ko
 insmod /lib/modules/fd-alsa-drv.ko
-_fd-dsp
+_fd-dsp &
+sleep 4
+fd-dsp-test &
